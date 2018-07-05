4da2829515e7bb35a4315c4b61f0da3593e4b2e7
Michael Morgan <michael1986morgan@gmail.com>
2018-07-04 23:00:59 -0400

    add file

---
+++ animalSounds.rb
@@ -0,0 +1,27 @@
+puts 'Enter a animal'
+userChoice = gets.chomp.downcase
+if userChoice == 'cat'
+  puts 'Meeeeooooowww'
+elsif userChoice == 'dog'
+  puts 'Whooooffff'
+elsif userChoice == 'pig'
+  puts 'oink'
+elsif userChoice == 'snake'
+  puts 'hiss'
+elsif userChoice == 'cow'
+  puts 'moooooo'
+else
+  puts "I don't know what #{userChoice} says."
+end
+
+# TURN THE IF/ELSE STATEMENT TO A CASE STATEMENT
+
+case userChoice
+  when 'cat' then puts 'Meow'
+  when 'dog' then puts 'whoof'
+  when 'pig' then puts 'oink'
+  when 'snake' then puts 'hiss'
+  when 'cow' then puts 'moooo'
+  else
+    puts "I don't know what #{userChoice} says."
+end
