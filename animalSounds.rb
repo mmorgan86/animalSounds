9bafb37ac838f04e249964b316ff94616f47fee9
Michael Morgan <michael1986morgan@gmail.com>
2018-07-04 23:22:22 -0400

    comment out if else statement

--- animalSounds.rb
+++ animalSounds.rb
@@ -1,18 +1,18 @@
 puts 'Enter a animal'
 userChoice = gets.chomp.downcase
-if userChoice == 'cat'
-  puts 'Meeeeooooowww'
-elsif userChoice == 'dog'
-  puts 'Whooooffff'
-elsif userChoice == 'pig'
-  puts 'oink'
-elsif userChoice == 'snake'
-  puts 'hiss'
-elsif userChoice == 'cow'
-  puts 'moooooo'
-else
-  puts "I don't know what #{userChoice} says."
-end
+# if userChoice == 'cat'
+#   puts 'Meeeeooooowww'
+# elsif userChoice == 'dog'
+#   puts 'Whooooffff'
+# elsif userChoice == 'pig'
+#   puts 'oink'
+# elsif userChoice == 'snake'
+#   puts 'hiss'
+# elsif userChoice == 'cow'
+#   puts 'moooooo'
+# else
+#   puts "I don't know what #{userChoice} says."
+# end
 
 # TURN THE IF/ELSE STATEMENT TO A CASE STATEMENT
 
